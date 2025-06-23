adfe33044f400419e3c7dd0fd68dd16e94e135f8
Nishantm032 <134581756+Nishantm032@users.noreply.github.com>
2025-06-23 21:47:44 +0530

    Update AboutPage.jsx

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AboutUs from '../components/Aboutus';
+import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
 import HappyCustomers from '../components/HappyCustomers';
 import StatsSection from '../components/statsSection';
